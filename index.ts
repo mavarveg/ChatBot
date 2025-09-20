b2d747205de9dc6d3fe3e1e627b58e32da8f8eba
Manuel <mavarg07@gmail.com>
2025-09-20 15:38:30 -0600

    Run both apps together

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+import concurrently from 'concurrently';
+
+concurrently([
+    { command: 'npm run dev --prefix packages/client', name: 'client', prefixColor: 'blue' },
+    { command: 'npm run dev --prefix packages/server', name: 'server', prefixColor: 'green' }
+]);
