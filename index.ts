73f6432c962361ef8badea1944df4ad95eb0fd46
Manuel <mavarg07@gmail.com>
2025-09-21 18:27:33 -0600

    Set up Prettier

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
 import concurrently from 'concurrently';
 
 concurrently([
-    { command: 'npm run dev --prefix packages/client', name: 'client', prefixColor: 'blue' },
-    { command: 'npm run dev --prefix packages/server', name: 'server', prefixColor: 'green' }
+   {
+      command: 'npm run dev --prefix packages/client',
+      name: 'client',
+      prefixColor: 'blue',
+   },
+   {
+      command: 'npm run dev --prefix packages/server',
+      name: 'server',
+      prefixColor: 'green',
+   },
 ]);
